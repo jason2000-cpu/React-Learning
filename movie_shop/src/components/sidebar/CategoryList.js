9ae03cbf6fc0a1293acaffa307ce6aa09cc8190d
jason2000-cpu <jasonmwangi2000@gmail.com>
2024-03-07 20:14:57 +0300

    CategoryList.js file

--- movie_shop/src/components/sidebar/CategoryList.js
+++ movie_shop/src/components/sidebar/CategoryList.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 function CategoryList({ category }) {
-    console.log("CATEGORY:::",category)
   return (
     <li 
         className='mx-1 my-2 h-10 flex items-center px-1 hover:bg-[#333551 text-white font-[sans-serif]'>
