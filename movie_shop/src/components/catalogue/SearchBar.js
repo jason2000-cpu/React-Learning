9f9778804aa507e2fdad81761c2deb01339b758b
jason2000-cpu <jasonmwangi2000@gmail.com>
2024-03-09 14:03:28 +0300

    added fetching from TMDB API

--- movie_shop/src/components/catalogue/SearchBar.js
+++ movie_shop/src/components/catalogue/SearchBar.js
@@ -5,27 +5,36 @@ import React, { useContext } from 'react'
 import { AppContext } from '../app';
 
 import { IoSearch } from "react-icons/io5";
+import { FaHandHoldingUsd } from 'react-icons/fa';
 
 
 function SearchBar() {
+    const formData = {
+        "searchQuery": ""
+    }
+
     const handleSearch = () => {
         alert(context.searchQuery)
     }
 
     const context = useContext(AppContext);
-
+    const handlesubmit = () => {
+        alert("form Submited", formData.searchQuery)
+        console.log()
+    }
   return (
     <div className='flex justify-between px-10 w-[75vw] border-b-[3px] border-[#5d5e89] h-16 my-4 mx-8 items-center'>
-        <h2 className='text-[#8289e7] text-[25px]'>New Movies</h2>
+        <h2 className='text-[#8289e7] text-[25px]'>{context.searchQuery || "New Movies"}</h2>
         <div className='flex'>
-            <input 
-                type='text' 
-                value={context.searchQuery}
-                className='h-8 w-72 dark:bg-[#3d4063] border border-black rounded' 
-                placeholder='Search'
-                onChange={(e)=>{context.setSearchQuery(e.target.value)}} 
-                
-                />
+            <form onSubmit={handlesubmit}>
+                <input 
+                    type='text' 
+                    className='h-8 w-72 dark:bg-[#3d4063] border border-black rounded' 
+                    placeholder='Search'
+                    onChange={(e)=>{formData.searchQuery[e.target.value]}} 
+                    
+                    />
+            </form>
             <IoSearch  
                 className='relative top-50 right-8 rounded-sm cursor-pointer'
                 size={32} 
--- movie_shop/src/components/sidebar/CategoryList.js
+++ movie_shop/src/components/sidebar/CategoryList.js
@@ -2,15 +2,22 @@ import Link from "next/link";
 import React, { useContext } from "react";
 import { AppContext } from "../app";
 
+import MovieCatalogue from "../catalogue/MovieCatalogue";
+
 function CategoryList({ category }) {
 
   const context = useContext(AppContext);
 
+  const handleClick = ()=>{
+    context.setSearchQuery(category); 
+    return (<MovieCatalogue title={category} />)
+  }
+
   return (
     <li
         name={category}
-        onClick={()=>{context.setSearchQuery(category); alert(context.searchQuery)}}
-        className='mx-1 my-2 h-10 flex items-center px-1  dark:text-white font-[sans-serif] cursor-pointer'>
+        onClick={handleClick}
+        className='mx-1 my-2 h-10 flex items-center px-1  dark:text-white font-[sans-serif] cursor-pointer hover:bg-gray-100 hover:text-[#8289e7] hover:rounded'>
         <a href={'#'} className='tracking-wide '>{category}</a>
     </li>
   )
